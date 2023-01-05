e5d6e2b915fc29d66d042b76fd4f5849d898d1aa
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-05 23:00:31 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -7,6 +7,58 @@ namespace InventoryManagementDemo
         public static void Main(string[] args)
         {
             Console.WriteLine("***** Welcome to the Object Oriented Program *****");
+
+            string path = "C:\\Users\\ganesh\\source\\repos\\InventoryManagementDeno\\InventoryManagementDeno\\Inventory.json";
+            ReadData obj = new ReadData();
+            var Info = obj.Read(path);
+
+            //Console.WriteLine(Info.typesOfRice.name);
+            //Console.WriteLine(Info.typesOfRice.weight);
+            //Console.WriteLine(Info.typesOfRice.price);
+
+            //Storing the multiple values of Array in List.
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Types of Rice is:");
+            Console.WriteLine("--------------------");
+            for (int i = 0; i < Info.typesOfRice.Count; i++)
+            {
+                Console.WriteLine(Info.typesOfRice[i].name);
+                Console.WriteLine(Info.typesOfRice[i].weight);
+                Console.WriteLine(Info.typesOfRice[i].price);
+
+               
+            }
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Types of Pulses is:");
+            Console.WriteLine("--------------------");
+
+
+            for (int i = 0; i < Info.typesOfPulses.Count; i++)
+            {
+                Console.WriteLine(Info.typesOfPulses[i].name);
+                Console.WriteLine(Info.typesOfPulses[i].weight);
+                Console.WriteLine(Info.typesOfPulses[i].price);
+
+                
+
+            }
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine("Types of Wheat is:");
+            Console.WriteLine("--------------------");
+
+            for (int i = 0; i < Info.typesOfWheat.Count; i++)
+            {
+                Console.WriteLine(Info.typesOfWheat[i].name);
+                Console.WriteLine(Info.typesOfWheat[i].weight);
+                Console.WriteLine(Info.typesOfWheat[i].price);
+
+                
+
+
+            }
         }
     }
 }
