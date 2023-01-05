60b10490d8132728e2119748ecab8626dede0bbc
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-05 22:52:46 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using InventoryManagementDeno;
+
+namespace InventoryManagementDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("***** Welcome to the Object Oriented Program *****");
+        }
+    }
+}
